65c7ca9c62f921383763aba4ff3eb4636ade810c
Christos Koumenides <chriskmnds@gmail.com>
2016-08-27 17:23:50 +0300

    updated specs

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -13,31 +13,52 @@ describe('Roman Numeral Converter tests.', function() {
   });
   
   [null, ''].forEach(function(value) {
-    it(value + ' should throw value required error', function() {
+    it('Testing ' + value + ': should throw value required error', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("value required"));
     });
   });
   
   ['IIII', 'MMMMCMXCIX', 'MMMMDMXCIX', 'error', 'CD1X', '1473'].forEach(function(value) {
-    it(value + ' should throw invalid value', function() {
+    it('Testing ' + value + ': should throw invalid value', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("invalid value"));
     });
   });
   
   [0, 10000].forEach(function(value) {
-    it(value + ' should throw invalid range', function() {
+    it('Testing ' + value + ': should throw invalid range', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("invalid range"));
     });
   });
   
-  it('3', function() {
-    var app = RomanNumber(3);
-    expect(app.toString()).toEqual('III');
-  });
-  
-    it('3', function() {
-    var app = RomanNumber(3);
-    expect(app.toInt()).toEqual(3);
+  // valid values and expected toInt/String results
+  var validValues = [
+    {value:1, expected:{toInt:1,toString:'I'}},
+    {value:3, expected:{toInt:3,toString:'III'}},
+    {value:4, expected:{toInt:4,toString:'IV'}},
+    {value:5, expected:{toInt:5,toString:'V'}},
+    {value:1968, expected:{toInt:1968,toString:'MCMLXVIII'}},
+    {value:2999, expected:{toInt:2999,toString:'MMCMXCIX'}},
+    {value:3000, expected:{toInt:3000,toString:'MMM'}},
+    {value:'I', expected:{toInt:1,toString:'I'}},
+    {value:'III', expected:{toInt:3,toString:'III'}},
+    {value:'IV', expected:{toInt:4,toString:'IV'}},
+    {value:'V', expected:{toInt:5,toString:'V'}},
+    {value:'CDXXIX', expected:{toInt:429,toString:'CDXXIX'}},
+    {value:'MCDLXXXII', expected:{toInt:1482,toString:'MCDLXXXII'}},
+    {value:'MCMLXXX', expected:{toInt:1980,toString:'MCMLXXX'}},
+  ];
+
+  validValues.forEach(function(v) {
+    describe('Testing ' + v.value + ':', function() {
+      it('toString method should return ' + v.expected.toString, function() {
+        var app = RomanNumber(v.value);
+        expect(app.toString()).toEqual(v.expected.toString);
+      });
+      it('toInt method should return ' + v.expected.toInt, function() {
+        var app = RomanNumber(v.value);
+        expect(app.toInt()).toEqual(v.expected.toInt);
+      });
+    });
   });
   
 });
