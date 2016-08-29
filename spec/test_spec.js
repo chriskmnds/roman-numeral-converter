e222b26861a8a95e5f950ac0c8bdd1bf5c403883
Christos Koumenides <chriskmnds@gmail.com>
2016-08-29 16:46:50 +0300

    cleanup

--- spec/test_spec.js
+++ spec/test_spec.js
@@ -1,35 +1,35 @@
 var RomanNumber = require('../lib/roman_number');
 
 describe('Roman Numeral Converter tests.', function() {
-  
+
   it('Calling with "new" should pass through and return object', function() {
     var app = new RomanNumber(123);
     expect(app).toEqual(jasmine.any(Object));
   });
-  
+
   it('Calling without "new" should pass through and return object', function() {
     var app = RomanNumber(123);
     expect(app).toEqual(jasmine.any(Object));
   });
-  
+
   [null, ''].forEach(function(value) {
     it('Testing ' + value + ': should throw value required error', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("value required"));
     });
   });
-  
+
   ['IIII', 'MMMMCMXCIX', 'MMMMDMXCIX', 'error', 'CD1X', '1473'].forEach(function(value) {
     it('Testing ' + value + ': should throw invalid value', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("invalid value"));
     });
   });
-  
+
   [0, 10000].forEach(function(value) {
     it('Testing ' + value + ': should throw invalid range', function() {
       expect(function(){ RomanNumber(value); }).toThrow(new Error("invalid range"));
     });
   });
-  
+
   // valid values and expected toInt/String results
   var validValues = [
     {value:1, expected:{toInt:1,toString:'I'}},
@@ -60,5 +60,5 @@ describe('Roman Numeral Converter tests.', function() {
       });
     });
   });
-  
+
 });
